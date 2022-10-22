af1e8cf3a2ffd2c31c9606ce492d203abab8bef6
Zenky <bzenky@gmail.com>
2022-10-22 16:06:48 -0300

    feat: dynamic product page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ interface HomeProps {
     id: string
     name: string
     imageUrl: string
-    price: number
+    price: string
   }[]
 }
 
@@ -29,7 +29,7 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Link key={product.id} href={`/product/${product.id}`}>
+          <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
             <Product className="keen-slider__slide">
               <Image src={product.imageUrl} width={520} height={480} alt="" />
               <footer>
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,21 +1,36 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
-import { useRouter } from 'next/router'
-import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
 
-export default function Product() {
-  const { query } = useRouter()
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails
+} from '../../styles/pages/product'
 
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
+
+export default function Product({ product }: ProductProps) {
   return (
     <ProductContainer>
       <ImageContainer>
-
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
       </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur asperiores dicta laborum eveniet nemo delectus hic. Repudiandae libero, sint, accusantium atque earum necessitatibus ab temporibus, quidem rem reprehenderit ducimus quis?</p>
+        <p>{product.description}</p>
 
         <button>
           Comprar agora
@@ -23,4 +38,39 @@ export default function Product() {
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { id: 'prod_MdhKzTBghl5KXo' } }
+    ],
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount / 100),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 1 // 1 hour
+  }
 }
