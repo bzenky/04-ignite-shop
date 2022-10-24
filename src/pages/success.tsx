e437e3860daa8216cbe1cbb684d7995f2f505bc4
Zenky <bzenky@gmail.com>
2022-10-24 20:24:53 -0300

    feat: ssr redirect

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -34,7 +34,17 @@ export default function Success({ customerName, product }: SuccessProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
   const sessionId = String(query.session_id)
+
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
     expand: ['line_items', 'line_items.data.price.product']
